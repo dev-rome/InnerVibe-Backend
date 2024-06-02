74c29aeac8c5308f980ff31577e17f8de7f3d4b2
dev-rome <84602714+dev-rome@users.noreply.github.com>
2024-06-02 18:39:17 -0400

    add get route and update routes with user and authmiddleware

--- src/routes/entryRoutes.js
+++ src/routes/entryRoutes.js
@@ -1,13 +1,24 @@
 const express = require("express");
-const Mood = require("../models/entryModel");
+const Entry = require("../models/entryModel");
+const authMiddleware = require("../middleware/authMiddleware");
 const router = express.Router();
 
-router.post("/", async (req, res) => {
+router.get("/", authMiddleware, async (req, res) => {
+    try {
+        const userEntries = await Entry.find({ user: req.user.id });
+        res.json(userEntries);
+    } catch (err) {
+        res.status(500).json({ msg: "Server error" });
+    }
+});
+
+router.post("/", authMiddleware, async (req, res) => {
     const { entry, title } = req.body;
 
     try {
-        const newEntry = new Mood({ entry, title });
-        await newEntry.save();
+        const newEntry = new Entry({ user: req.user.id, entry, title });
+        const savedEntry = await newEntry.save();
+        res.json(savedEntry);
         res.status(201).json({ message: "Mood recorded successfully" });
     } catch (err) {
         res.status(500).json({ err: "Failed to record entry" });
