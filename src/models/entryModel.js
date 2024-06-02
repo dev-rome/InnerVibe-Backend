8ee4492d88e5bce084618d5cbe09604b5e1b13fa
dev-rome <84602714+dev-rome@users.noreply.github.com>
2024-06-02 18:38:27 -0400

    update entry with user

--- src/models/entryModel.js
+++ src/models/entryModel.js
@@ -4,6 +4,11 @@ const entrySchema = new mongoose.Schema({
     entry: { type: String, required: true },
     timestamp: { type: Date, default: Date.now },
     title: { type: String, required: true },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    }
 });
 
 module.exports = mongoose.model("Entry", entrySchema);
